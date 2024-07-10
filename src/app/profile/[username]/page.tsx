985f1594c8bdf2bd2aae12d7c6d1a0143fd51f19
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-10 22:54:34 +0500

    fetching-data

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,7 +2,9 @@ import Feed from "@/components/Feed";
 import LeftMenu from "@/components/LeftMenu";
 import RightMenu from "@/components/RightMenu";
 import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export const ProfilePage = async ({
   params,
@@ -15,7 +17,38 @@ export const ProfilePage = async ({
     where: {
       username,
     },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          followings: true,
+          posts: true,
+        },
+      },
+    },
   });
+
+  if (!user) return notFound();
+
+  const { userId: currentUserId } = auth();
+
+  let isBlocked;
+
+  if (currentUserId) {
+    const res = await prisma.block.findFirst({
+      where: {
+        blockerId: user.id,
+        blockedId: currentUserId,
+      },
+    });
+
+    if (res) isBlocked = true;
+  } else {
+    isBlocked = false;
+  }
+
+  if (isBlocked) return notFound();
+
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
@@ -26,34 +59,38 @@ export const ProfilePage = async ({
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
               <Image
-                src="https://images.pexels.com/photos/23825646/pexels-photo-23825646/free-photo-of-iceland-northern-lights.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                src={user.cover || "/noCover.png"}
                 alt=""
                 fill
                 className="rounded-md object-cover"
               />
 
               <Image
-                src="https://images.pexels.com/photos/26746886/pexels-photo-26746886/free-photo-of-cowboy-riding-a-horse-towards-a-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                src={user.avatar || "/noAvatar.png"}
                 alt=""
                 width={128}
                 height={128}
                 className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
               />
             </div>
-            <h1 className="mt-20 mb-4 text-2xl font-medium">John Doe</h1>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              {user.name && user.surname
+                ? user.name + " " + user.surname
+                : user.username}
+            </h1>
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">{user._count.posts}</span>
                 <span className="text-sm">Posts</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">1.2M</span>
+                <span className="font-medium">{user._count.followers}</span>
                 <span className="text-sm">Followers</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">100</span>
+                <span className="font-medium">{user._count.followings}</span>
                 <span className="text-sm">Following</span>
               </div>
             </div>
@@ -62,7 +99,7 @@ export const ProfilePage = async ({
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
-        <RightMenu userId="test" />
+        <RightMenu user={user} />
       </div>
     </div>
   );
--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { User } from "@prisma/client";
 
-const UserMediaCard = ({ userId }: { userId: string }) => {
+const UserMediaCard = ({ user }: { user?: User }) => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* top */}
