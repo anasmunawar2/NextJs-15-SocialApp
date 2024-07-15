8e935e84638c6e75192aa9e2b76ccbc3818bbb76
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-16 01:20:57 +0500

    follow-request

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,8 +1,26 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import FriendRequestList from "./rightMenu/FriendRequestList";
 
-const FriendRequests = () => {
+const FriendRequests = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const requests = await prisma.followRequest.findMany({
+    where: {
+      receiverId: userId,
+    },
+    include: {
+      sender: true,
+    },
+  });
+
+  if (requests.length === 0) return null;
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* top */}
@@ -13,92 +31,7 @@ const FriendRequests = () => {
         </Link>
       </div>
       {/* user */}
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <Image
-            src="https://images.pexels.com/photos/25478461/pexels-photo-25478461/free-photo-of-men-girl.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={40}
-            height={40}
-            className="w-10 h-10 rounded-full object-cover"
-          />
-          <span className="font-semibold gap-4 ">John Doe</span>
-        </div>
-        <div className="flex gap-3  justify-end">
-          <Image
-            src="/accept.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-          <Image
-            src="/reject.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-        </div>
-      </div>
-
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <Image
-            src="https://images.pexels.com/photos/25478461/pexels-photo-25478461/free-photo-of-men-girl.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={40}
-            height={40}
-            className="w-10 h-10 rounded-full object-cover"
-          />
-          <span className="font-semibold gap-4 ">John Doe</span>
-        </div>
-        <div className="flex gap-3  justify-end">
-          <Image
-            src="/accept.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-          <Image
-            src="/reject.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-        </div>
-      </div>
-
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <Image
-            src="https://images.pexels.com/photos/25478461/pexels-photo-25478461/free-photo-of-men-girl.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-            width={40}
-            height={40}
-            className="w-10 h-10 rounded-full object-cover"
-          />
-          <span className="font-semibold gap-4 ">John Doe</span>
-        </div>
-        <div className="flex gap-3  justify-end">
-          <Image
-            src="/accept.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-          <Image
-            src="/reject.png"
-            alt=""
-            width={20}
-            height={20}
-            className="cursor-pointer "
-          />
-        </div>
-      </div>
+      <FriendRequestList requests={requests} />
     </div>
   );
 };
