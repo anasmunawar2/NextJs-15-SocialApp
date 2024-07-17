1946fb578ce39e07febf5df7766b91a66836e049
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-17 23:52:01 +0500

    zod installed

--- src/components/rightMenu/updateUser.tsx
+++ src/components/rightMenu/updateUser.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { updateProfile } from "@/lib/actions";
 import { User } from "@prisma/client";
 import Image from "next/image";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const UpdateUser = ({ user }: { user: User }) => {
   const [open, setOpen] = useState(false);
@@ -21,8 +22,8 @@ const UpdateUser = ({ user }: { user: User }) => {
       {open && (
         <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50">
           <form
-            action=""
-            className="bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
+            action={updateProfile}
+            className="p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
           >
             {/* Title */}
             <h1>Update Profile</h1>
@@ -55,6 +56,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.name || "John"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="name"
                 />
               </div>
 
@@ -68,6 +70,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.surname || "Doe"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="surname"
                 />
               </div>
 
@@ -81,6 +84,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.description || "Hello World..."}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="description"
                 />
               </div>
 
@@ -94,6 +98,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.city || "New York"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="city"
                 />
               </div>
 
@@ -107,6 +112,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.school || "UET"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="school"
                 />
               </div>
 
@@ -120,6 +126,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                   type="text"
                   placeholder={user.work || "Apple Inc."}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="work"
                 />
               </div>
 
@@ -135,6 +142,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                     user.website || "https://anasmunawar.netlify.app/"
                   }
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
+                  name="website"
                 />
               </div>
             </div>
