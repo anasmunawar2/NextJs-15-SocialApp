c2fd5b4c49e68728ed81a51c451776330763fe7e
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-24 22:02:30 +0500

    commentList

--- src/components/feed/CommentList.tsx
+++ src/components/feed/CommentList.tsx
@@ -1,49 +1,98 @@
 "use client";
+import { addComment } from "@/lib/actions";
+import { useUser } from "@clerk/nextjs";
 import { Comment, User } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
+import { comment } from "postcss";
+import React, { useOptimistic, useState } from "react";
 
 type CommentWithUser = Comment & { user: User };
 const CommentList = ({
   comments,
   postId,
 }: {
-  comments: CommentWithUser;
+  comments: CommentWithUser[];
   postId: number;
 }) => {
+  const { user } = useUser();
+  const [commentState, setCommentState] = useState(comments);
+  const [desc, setDesc] = useState("");
+
+  const add = async () => {
+    if (!user || !desc) return;
+
+    addOptimisticComment({
+      id: Math.random(),
+      desc,
+      createdAt: new Date(Date.now()),
+      updatedAt: new Date(Date.now()),
+      userId: user.id,
+      postId: postId,
+      user: {
+        id: user.id,
+        username: "Sending Please Wait...",
+        avatar: user.imageUrl || "/noAvatar.png",
+        cover: "",
+        description: "",
+        name: "",
+        surname: "",
+        city: "",
+        work: "",
+        school: "",
+        website: "",
+        createdAt: new Date(Date.now()),
+      },
+    });
+    try {
+      const createdComment = await addComment(postId, desc);
+      setCommentState((prev) => [createdComment, ...prev]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const [optimisticComments, addOptimisticComment] = useOptimistic(
+    commentState,
+    (state, value: CommentWithUser) => [value, ...state]
+  );
   return (
     <>
-      <div className="flex items-center gap-4">
-        <Image
-          src="https://images.pexels.com/photos/6769533/pexels-photo-6769533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-          width={32}
-          height={32}
-          className="w-8 h-8 rounded-full"
-        />
-        <div className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
-          <input
-            type="text"
-            placeholder="Write a comment..."
-            className="bg-transparent outline-none flex-1"
-          />
+      {user && (
+        <div className="flex items-center gap-4">
           <Image
-            src="/emoji.png"
-            width={16}
-            height={16}
+            src={user.imageUrl || "noAvatar.png"}
             alt=""
-            className="cursor-pointer"
+            width={32}
+            height={32}
+            className="w-8 h-8 rounded-full"
           />
+          <form
+            action={add}
+            className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full"
+          >
+            <input
+              type="text"
+              placeholder="Write a comment..."
+              className="bg-transparent outline-none flex-1"
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <Image
+              src="/emoji.png"
+              width={16}
+              height={16}
+              alt=""
+              className="cursor-pointer"
+            />
+          </form>
         </div>
-      </div>
+      )}
       {/* comments */}
-      <div className="">
-        {/* comment */}
-        <div className="flex gap-4  justify-between mt-6">
+      {optimisticComments.map((comment) => (
+        <div className="flex gap-4  justify-between mt-6" key={comment.id}>
           {/* avatar */}
 
           <Image
-            src="https://images.pexels.com/photos/6769533/pexels-photo-6769533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            src={comment.user.avatar || "noAvatar.png"}
             alt=""
             width={40}
             height={40}
@@ -52,13 +101,12 @@ const CommentList = ({
 
           {/* desc */}
           <div className="flex flex-col gap-2 flex-1">
-            <span className="font-medium">John Doe</span>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters.
-            </p>
+            <span className="font-medium">
+              {comment.user.name && comment.user.surname
+                ? comment.user.name + "" + comment.user.surname
+                : comment.user.username}
+            </span>
+            <p>{comment.desc}</p>
             <div className="flex  items-center gap-8 text-xs text-gray-500 mt-2">
               <div className="flex items-center gap-4">
                 <Image
@@ -69,7 +117,7 @@ const CommentList = ({
                   height={12}
                 />
                 <span className="text-gray-300">|</span>
-                <span className="text-gray-500">123 Likes</span>
+                <span className="text-gray-500">0</span>
               </div>
 
               <div className="">Reply</div>
@@ -84,7 +132,7 @@ const CommentList = ({
             height={16}
           />
         </div>
-      </div>
+      ))}
     </>
   );
 };
